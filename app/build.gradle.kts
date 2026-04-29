9d8d3cf541c8b9bf9ed0f016f641e8e7e892ee3e
pa-nov <novpavel06@gmail.com>
2026-04-29 20:15:00 +0300

    build: update build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,16 +9,20 @@ android {
 
     defaultConfig {
         applicationId = "com.panov.proxy"
+
         minSdk = 26
         targetSdk = 36
+
         versionCode = 1
         versionName = "0.0.1"
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
 
