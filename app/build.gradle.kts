cda0eac7dcb3e02124c7387733dc0fd5b08d1bc6
pa-nov <novpavel06@gmail.com>
2026-04-29 17:45:19 +0300

    chore(deps): update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,14 +37,15 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
 
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.okhttp)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
