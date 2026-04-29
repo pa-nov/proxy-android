d3e73a2719f452e9cb90ae80f16d741bb0ea324e
pa-nov <novpavel06@gmail.com>
2026-04-29 03:03:45 +0300

    feat(ui): add header screen component

---
+++ app/src/main/kotlin/com/panov/proxy/core/components/HeaderScreen.kt
@@ -0,0 +1,123 @@
+package com.panov.proxy.core.components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.visible
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.panov.proxy.R
+import com.panov.proxy.core.theme.ProxyTheme
+
+@Composable
+fun HeaderScreen(
+    navigator: NavHostController,
+    title: String = "",
+    onClickMore: (() -> Unit)? = null,
+    spacing: Dp = 16.dp,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    val scrollState = rememberScrollState()
+    val isScrolling by remember {
+        derivedStateOf {
+            scrollState.value > 0
+        }
+    }
+    val headerColor by animateColorAsState(
+        targetValue = if (isScrolling) {
+            MaterialTheme.colorScheme.surfaceVariant
+        } else {
+            MaterialTheme.colorScheme.background
+        }
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .navigationBarsPadding()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(headerColor)
+                .statusBarsPadding()
+                .padding(16.dp)
+        ) {
+            SmallButton(
+                onClick = {
+                    navigator.popBackStack()
+                }, painter = painterResource(R.drawable.icon_arrow_left)
+            )
+            Text(
+                text = title,
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically),
+                color = MaterialTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+            if (onClickMore != null) {
+                SmallButton(
+                    onClick = onClickMore, painter = painterResource(R.drawable.icon_more)
+                )
+            } else {
+                SmallButton(
+                    onClick = {},
+                    modifier = Modifier.visible(false),
+                    enabled = false,
+                    painter = painterResource(R.drawable.icon_more)
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(spacing),
+            content = content
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewHeaderScreen() {
+    ProxyTheme {
+        HeaderScreen(
+            navigator = NavHostController(LocalContext.current),
+            title = "Title",
+            onClickMore = {},
+            spacing = 16.dp
+        ) {
+            repeat(16) {
+                WideButton({})
+            }
+        }
+    }
+}
