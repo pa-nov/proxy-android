bf31549bb1f804f684ee28117c3b994a79a139dd
pa-nov <novpavel06@gmail.com>
2026-06-06 05:09:39 +0300

    chore: bump version to 0.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 9
+        versionName = "0.0.9"
     }
 
     buildTypes {
